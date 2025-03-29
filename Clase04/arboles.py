71f125a2bca8988b57da651220da1f3c60df3865
GonzaBolognese <gonza.1703@gmail.com>
2025-03-29 00:07:17 -0300

    ejercicios clase04 finalizados

--- Clase04/arboles.py
+++ Clase04/arboles.py
@@ -1,58 +1,119 @@
 import csv
 from collections import Counter
 
+RUTA_ARCHIVO = r"C:\Users\gonza\OneDrive\Escritorio\Programacion_1\Ejercicios\programacion1-unsam\data\arbolado-en-espacios-verdes.csv"
+
+
 def leer_parque(nombre_archivo, parque):
     lista_parque = []
-    ruta = rf'U:\Gonzalo\Documentos\Prs\programacion\programacion1-unsam\Data\{nombre_archivo}'
-    f = open(ruta, encoding="utf-8")
-    filas = csv.reader(f)
-    headers = next(filas)
-    indice_filtro = headers.index('espacio_ve')
+    try:
+        ruta = f'{nombre_archivo}'
+        f = open(ruta, encoding="utf-8")
+        filas = csv.reader(f)
+        headers = next(filas)
+        indice_filtro = headers.index('espacio_ve')
 
-    for i, fila in enumerate(filas, start=1):
-        if(fila[indice_filtro] == parque):
-            lista_parque.append(dict(zip(headers, fila)))
+        for i, fila in enumerate(filas, start=1):
+            if(fila[indice_filtro] == parque):
+                lista_parque.append(dict(zip(headers, fila)))
+    except:
+        print('Faltan valores en el archivo ingresado')
     return lista_parque
 
 def especies(lista_arboles):
     especies = set()
-    for l in lista_arboles:
-        if l['nombre_com'] != 'No Determinado' and l['nombre_com'] != 'No Determinable':
-            especies.add(l['nombre_com'])
-    print(especies)
+    try:
+        for l in lista_arboles:
+            if l['nombre_com'] != 'No Determinado' and l['nombre_com'] != 'No Determinable':
+                especies.add(l['nombre_com'])
+        return especies
+    except:
+        print('Faltan valores en el archivo ingresado')
 
 def contar_ejemplares(lista_arboles):
     lista = Counter()
-    for l in lista_arboles:
-        if l['nombre_com'] != 'No Determinado' and l['nombre_com'] != 'No Determinable':
-            lista[l['nombre_com']] += 1
-    return lista.most_common(5)
+    try:
+        for l in lista_arboles:
+            if l['nombre_com'] != 'No Determinado' and l['nombre_com'] != 'No Determinable':
+                lista[l['nombre_com']] += 1
+        return lista.most_common(5)
+    except:
+        print('Faltan valores en el archivo ingresado')
 
 def obtener_alturas(lista_arboles, especie):
     max = 0
     lista_Alturas = []
+    try:
+        for l in lista_arboles:
+            if l['nombre_com'] == especie:
+                if int(l['altura_tot']) > max:
+                    max = int(l['altura_tot'])
+                lista_Alturas.append(int(l['altura_tot']))
+        promedio = sum(lista_Alturas)/len(lista_Alturas)
 
-    for l in lista_arboles:
-        if l['nombre_com'] == especie:
-            if int(l['altura_tot']) > max:
-                max = int(l['altura_tot'])
-            lista_Alturas.append(int(l['altura_tot']))
-    promedio = sum(lista_Alturas)/len(lista_Alturas)
+        return(max, promedio)
+    except:
+        print('Faltan valores en el archivo ingresado')
 
-    return(max, promedio)
+def obtener_inclinaciones(lista_arboles, especie):
+    lista_Inclinacion = set()
+    try:
+        for l in lista_arboles:
+            if l['nombre_com'] == especie:
+                lista_Inclinacion.add(int(l['inclinacio']))
+        return(lista_Inclinacion)
+    except:
+        print('Faltan valores en el archivo ingresado')
 
+def especimen_mas_inclinado(lista_arboles):
+    max_incli = dict()
+    especies_unicas = especies(lista_arboles)
+    mas_inclinado={'especie': '', 'inclinacion': 0}
+    try:
+        for e in especies_unicas:
+            incli = obtener_inclinaciones(lista_arboles, e)
+            max = 0
+            for i in incli:
+                if i>max:
+                    max = i
+            max_incli[e] = max
+        for k in max_incli:
+            if max_incli[k] > mas_inclinado['inclinacion']:
+                mas_inclinado['especie'] = k
+                mas_inclinado['inclinacion'] = max_incli[k]
+        return (mas_inclinado['especie'], f'{mas_inclinado['inclinacion']}')
+    except:
+        print('Faltan valores en el archivo ingresado')
 
+def especie_promedio_mas_inclinada(lista_arboles):
+    prom_incli = dict()
+    especies_unicas = especies(lista_arboles)
+    especie_mas_inclinada = {'especie': '', 'inclinacion': 0}
+    try:
+        for e in especies_unicas:
+            incli = obtener_inclinaciones(lista_arboles, e)
+            prom = round(sum(incli)/len(incli), 2)
+            prom_incli[e] = prom
+        for k in prom_incli:
+            if prom_incli[k] > especie_mas_inclinada['inclinacion']:
+                especie_mas_inclinada['especie'] = k
+                especie_mas_inclinada['inclinacion'] = prom_incli[k]
+        return f'Los {especie_mas_inclinada["especie"]} del Parque {lista_arboles[0]['espacio_ve'].lower()} tiene un promedio de inclinación de {especie_mas_inclinada["inclinacion"]} grados.'
+    except:
+        print('Faltan valores en el archivo ingresado')
 
 
 data = {
-    "General Paz": obtener_alturas(leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ"), 'Jacarandá'),
-    "Los Andes": obtener_alturas(leer_parque("arbolado-en-espacios-verdes.csv", "ANDES, LOS"), 'Jacarandá'),
-    "Centenario": obtener_alturas(leer_parque("arbolado-en-espacios-verdes.csv", "CENTENARIO"), 'Jacarandá')
+    "General Paz": obtener_alturas(leer_parque(RUTA_ARCHIVO, "GENERAL PAZ"), 'Jacarandá'),
+    "Los Andes": obtener_alturas(leer_parque(RUTA_ARCHIVO, "ANDES, LOS"), 'Jacarandá'),
+    "Centenario": obtener_alturas(leer_parque(RUTA_ARCHIVO, "CENTENARIO"), 'Jacarandá')
 }
 
+"""
 print("=" * 120)
 print(f"{'Descripcion':<30s}{'General Paz':<30s}{'Los Andes':<30s}{'Centenario':<30s}")
 print("=" * 120)
+"""
 
 
 # Imprimir filas
@@ -65,7 +126,17 @@ for i in range(5):
 
 print("=" * 90)
 """
-
+"""
 print(f'{'max':<30s}{data["General Paz"][0]:<30d} {data["Los Andes"][0]:<30d} {data["Centenario"][0]:<30d}')
 print(f'{'prom':<30s}{data["General Paz"][1]:<30.2f} {data["Los Andes"][1]:<30.2f} {data["Centenario"][1]:<30.2f}')
 print("=" * 120)
+"""
+
+#print(obtener_inclinaciones(leer_parque(RUTA_ARCHIVO, "GENERAL PAZ"), 'Jacarandá'))
+#print (especimen_mas_inclinado(leer_parque(RUTA_ARCHIVO, "GENERAL PAZ")))
+#print (especimen_mas_inclinado(leer_parque(RUTA_ARCHIVO, "ANDES, LOS")))
+#print (especimen_mas_inclinado(leer_parque(RUTA_ARCHIVO, "CENTENARIO")))
+
+print (especie_promedio_mas_inclinada(leer_parque(RUTA_ARCHIVO, "GENERAL PAZ")))
+print (especie_promedio_mas_inclinada(leer_parque(RUTA_ARCHIVO, "ANDES, LOS")))
+print (especie_promedio_mas_inclinada(leer_parque(RUTA_ARCHIVO, "CENTENARIO")))
